af1310ca80c2183c2084847bacde5a34d2e36ffb
eustachekamala <eustachejared25@gmail.com>
2024-10-01 10:20:14 +0300

    responses for the request made by the client

--- server/app.py
+++ server/app.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
-
-from flask import Flask
+import os
+from flask import Flask, request, current_app, g, make_response # type: ignore
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+     g.path = os.path.abspath(os.getcwd())
+     
+
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    appname = current_app.name
+    reponse_body = f'''<h1>The host for this page is {host}</h1>
+             <h2>The app name is {appname}</h2>
+             <h3> The Path of this appliction on the users's machine is {g.path}</h3>
+             <h4>The path of this file is {__file__}</h4>'''
+             
+    status_header = 200
+    header = {}
+    
+    return make_response(reponse_body, status_header, header)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
